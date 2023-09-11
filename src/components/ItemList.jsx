107189970544d4e7e1e4575177b891bc780920cd
German <german_gomez1205@hotmail.com>
2023-09-11 15:08:29 -0700

    ft. create, update and delete functions

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,13 +1,30 @@
+'use client'
 import React from 'react'
-import { Trash2 } from 'lucide-react';
+import { Trash2 } from '../../node_modules/lucide-react'
 
-const ItemList = () => {
+const ItemList = ({ task, index, handleComplete, handleDelete }) => {
   return (
-    <div className="max-w-7xl">
-      <div className="bg-[#526D82] h-auto w-full flex items-start rounded-2xl">
-        <div className="flex flex-row items-center justify-between w-auto h-full m-4">
-          <p>Item list of any task</p>
-          <Trash2 />
+    <div className='w-full h-auto px-4 py-2 animate-fade-left animate-duration-500 animate-ease-in-out'>
+      <div className='flex flex-row items-center justify-between w-auto h-full m-2'>
+        <p className='overflow-ellipsis'>{task.task}</p>
+        <div className='flex gap-2 ml-2'>
+          {task.completed === true ? (
+            <input
+              type='checkbox'
+              checked='false'
+              onChange={() => handleComplete(index)}
+              className='transition duration-300 transform cursor-pointer hover:scale-125'
+            />
+          ) : (
+            <input
+              type='checkbox'
+              onChange={() => handleComplete(index)}
+              className='transition duration-300 transform cursor-pointer hover:scale-125'
+            />
+          )}
+          <button onClick={() => handleDelete(index)}>
+            <Trash2 className='w-5 transition duration-300 transform hover:scale-125' />
+          </button>
         </div>
       </div>
     </div>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,28 +1,83 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import ItemList from '@/components/ItemList'
 
+const initialTasks = JSON.parse(localStorage.getItem('tasks')) || []
+const initialTask = {
+  task: '',
+  completed: false,
+}
+
 const List = () => {
+  const [task, setTask] = useState(initialTask)
+  const [tasks, setTasks] = useState(initialTasks)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    if (task.task === '') return
+
+    setTasks([...tasks, task])
+    setTask(initialTask)
+
+    event.target.reset()
+    localStorage.setItem('tasks', JSON.stringify([...tasks, task]))
+  }
+
+  const handleComplete = (index) => {
+    const newTasks = [...tasks]
+
+    if(newTasks[index].completed === true){
+      newTasks[index].completed = false
+      setTask(newTasks)
+      localStorage.setItem('tasks', JSON.stringify(tasks))
+      return
+    }    
+
+    newTasks[index].completed = true
+    setTasks(newTasks)
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  }
+
+  const handleDelete = (index) => {
+    const newTasks = [...tasks]
+    newTasks.splice(index, 1)
+    setTasks(newTasks)
+    localStorage.setItem('tasks', JSON.stringify(newTasks))
+  }
+
   return (
     <div className='max-w-7xl'>
       <div className='bg-[#526D82] h-auto w-full  items-center rounded-2xl'>
-        <div className='flex flex-col'>
-          <div className='flex flex-row justify-between items-center border-b-2 border-[#DDE6ED] h-full m-4 w-auto'>
-            <label htmlFor=''>
+        <form onSubmit={handleSubmit}>
+          <div className='flex flex-col'>
+            <div className='flex flex-row justify-between items-center border-b-2 border-[#DDE6ED] h-full m-4 w-auto'>
+              <label>
+                <input
+                  className='w-full pr-8 ml-2 bg-transparent focus:outline-none'
+                  type='text'
+                  placeholder='Add a new task...'
+                  onChange={event =>
+                    setTask({ task: event.target.value, completed: false })
+                  }
+                />
+              </label>
               <input
-                className='w-full pr-8 ml-2 bg-transparent focus:outline-none'
-                type='text'
-                placeholder='Add a new task...'
+                className='flex items-center h-8 bg-[#9DB2BF] px-2 font-light rounded-xl mb-2 mr-2 hover:bg-[#303f53] cursor-pointer'
+                type='submit'
+                value='Add'
               />
-            </label>
-            <button
-              className='flex items-center h-8 bg-[#9DB2BF] p-2 rounded-xl mb-2 mr-2 hover:bg-[#303f53]'
-              type='button'
-            >
-              Add
-            </button>
+            </div>
           </div>
-        </div>
-        <ItemList />
+        </form>
+        {tasks?.map((task, index) => (
+          <ItemList
+            key={index}
+            task={task}
+            index={index}
+            handleComplete={handleComplete}
+            handleDelete={handleDelete}
+          />
+        ))}
       </div>
     </div>
   )
